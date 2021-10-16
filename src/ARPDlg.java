1afe1b702a7a99295cd6dbdee8d8175fafb5dd26
songyi <ksl2950@naver.com>
2021-10-16 14:13:38 +0900

    feat : proxy update

--- src/ARPDlg.java
+++ src/ARPDlg.java
@@ -40,9 +40,9 @@ public class ARPDlg extends JFrame implements BaseLayer {
 	private JTextField dstIpWrite;
 	private JTextField proxyDeviceWrite;
 	private JTextField proxyIpWrite;
-	private JTextField proxyMacWrite; 
+	private JTextField proxyMacWrite;
 	private JTextField GArpAddressWrite;
-	
+
 	Container contentPane;
 
 	JTextArea ChattingArea;
@@ -59,18 +59,18 @@ public class ARPDlg extends JFrame implements BaseLayer {
 	JLabel proxyIp;
 	JLabel proxyMac;
 	JLabel GArpAddress;
-	
+
 	JButton Setting_Button;
 	JButton Chat_send_Button;
 	JButton File_send_Button;
 	JButton openFileButton;
 	JButton itemDeleteButton;
 	JButton allDeleteButton;
-	JButton dstIpSendButton; 
+	JButton dstIpSendButton;
 	JButton proxyAddButton;
 	JButton proxyDeleteButton;
 	JButton GArpSendButton;
-	
+
 	static JComboBox<String> NICComboBox;
 
 	int adapterNumber = 0;
@@ -89,8 +89,8 @@ public class ARPDlg extends JFrame implements BaseLayer {
 		m_LayerMgr.AddLayer(new ARPLayer("ARP"));
 		m_LayerMgr.AddLayer(new IPLayer("IP"));
 		m_LayerMgr.AddLayer(new TCPLayer("TCP"));
-		//m_LayerMgr.AddLayer(new ChatAppLayer("ChatApp"));
-		//m_LayerMgr.AddLayer(new FileAppLayer("FileApp"));
+		// m_LayerMgr.AddLayer(new ChatAppLayer("ChatApp"));
+		// m_LayerMgr.AddLayer(new FileAppLayer("FileApp"));
 		m_LayerMgr.AddLayer(new ARPDlg("GUI"));
 		m_LayerMgr.ConnectLayers(" NI ( *Ethernet ( *ARP ( *IP ( *TCP ( *GUI ) ) ) ) )");
 		///////////////////
@@ -111,12 +111,12 @@ public class ARPDlg extends JFrame implements BaseLayer {
 				} else {
 					byte[] MacAddress = new byte[6];
 					byte[] IpAddress = new byte[4];
-					
+
 					String srcMac = srcMacAddress.getText();
 					String srcIP = srcIpAddress.getText();
-					System.out.println("srcMacAddress : "+ srcMac);
-					System.out.println("srcIPAddress : "+ srcIP);
-					
+					System.out.println("srcMacAddress : " + srcMac);
+					System.out.println("srcIPAddress : " + srcIP);
+
 					String[] byte_srcMac = srcMac.split("-");
 					for (int i = 0; i < 6; i++) {
 						MacAddress[i] = (byte) Integer.parseInt(byte_srcMac[i], 16);
@@ -126,13 +126,15 @@ public class ARPDlg extends JFrame implements BaseLayer {
 					for (int i = 0; i < 4; i++) {
 						IpAddress[i] = (byte) Integer.parseInt(byte_srcIp[i]);
 					}
-					
+
 					srcIPNumber = IpAddress;
 					srcMacNumber = MacAddress;
-					
-					//((EthernetLayer) m_LayerMgr.GetLayer("Ethernet")).SetEnetSrcAddress(MacAddress);
-					//((EthernetLayer) m_LayerMgr.GetLayer("Ethernet")).SetEnetDstAddress(IpAddress);
-					
+
+					// ((EthernetLayer)
+					// m_LayerMgr.GetLayer("Ethernet")).SetEnetSrcAddress(MacAddress);
+					// ((EthernetLayer)
+					// m_LayerMgr.GetLayer("Ethernet")).SetEnetDstAddress(IpAddress);
+
 					((NILayer) m_LayerMgr.GetLayer("NI")).SetAdapterNumber(adapterNumber);
 
 					Setting_Button.setText("Reset");
@@ -141,24 +143,35 @@ public class ARPDlg extends JFrame implements BaseLayer {
 
 				}
 			}
-			// basic ARP 전송 
+			// basic ARP 전송
 			if (e.getSource() == dstIpSendButton) {
 				if (dstIpSendButton.getText() == "Send") {
 					String dstIP = dstIpWrite.getText();
 					cacheArea.append(dstIP);
 					cacheArea.append("  ??-??-??-??-??-??");
-					cacheArea.append("  sending.."+"\n");
+					cacheArea.append("  Incomplete" + "\n");
 					byte[] dstIPAddress = new byte[4];
 					String[] byte_dstIP = dstIP.split("\\.");
-					for (int i=0; i<4; i++) {
-						dstIPAddress[i] = (byte) Integer.parseInt(byte_dstIP[i],10);
+					for (int i = 0; i < 4; i++) {
+						dstIPAddress[i] = (byte) Integer.parseInt(byte_dstIP[i], 10);
 					}
 					dstIPNumber = dstIPAddress;
 					((TCPLayer) m_LayerMgr.GetLayer("TCP")).ARPSend(srcIPNumber, dstIPNumber);
-					
+
 				}
 			}
-			
+			// proxy ARP 전송
+			if (e.getSource() == proxyAddButton) {
+				if (proxyAddButton.getText() == "Add") {
+					String proxyDevice = proxyDeviceWrite.getText();
+					String proxyIP = proxyIpWrite.getText();
+					String proxyMac = proxyMacWrite.getText();
+					proxyArpArea.append("Interface0");
+					proxyArpArea.append("  " + proxyIP);
+					proxyArpArea.append("  " + proxyMac + "\n");
+				}
+			}
+
 			if (e.getSource() == Chat_send_Button) {
 				if (Setting_Button.getText() == "Reset") {
 					for (int i = 0; i < 10; i++) {
@@ -178,21 +191,21 @@ public class ARPDlg extends JFrame implements BaseLayer {
 				}
 			}
 			if (e.getSource() == openFileButton) {
-				FileNameExtensionFilter filter = new FileNameExtensionFilter("txt","txt");
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("txt", "txt");
 				JFileChooser chooser = new JFileChooser();
 				chooser.setFileFilter(filter);
 				int ret = chooser.showOpenDialog(null);
-				if(ret == JFileChooser.APPROVE_OPTION) {
+				if (ret == JFileChooser.APPROVE_OPTION) {
 					String filePath = chooser.getSelectedFile().getPath();
 					fileArea.setText(filePath);
 					File_send_Button.setEnabled(true);
 					file = chooser.getSelectedFile();
-					
+
 				}
-				
+
 			}
 			if (e.getSource() == File_send_Button) {
-				((FileAppLayer)m_LayerMgr.GetLayer("FileApp")).setAndStartSendFile();
+				((FileAppLayer) m_LayerMgr.GetLayer("FileApp")).setAndStartSendFile();
 				File_send_Button.setEnabled(false);
 			}
 		}
@@ -209,183 +222,123 @@ public class ARPDlg extends JFrame implements BaseLayer {
 		((JComponent) contentPane).setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		//ARP Cache panel 
+
+		// ARP Cache panel
 		JPanel arpCachePanel = new JPanel();
 		arpCachePanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "ARP Cache",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		arpCachePanel.setBounds(10,5, 370, 371);
+		arpCachePanel.setBounds(10, 5, 370, 371);
 		contentPane.add(arpCachePanel);
 		arpCachePanel.setLayout(null);
-		
+
 		JPanel arpCacheEditorPanel = new JPanel();
 		arpCacheEditorPanel.setBounds(10, 15, 350, 230);
 		arpCachePanel.add(arpCacheEditorPanel);
 		arpCacheEditorPanel.setLayout(null);
-		
+
 		cacheArea = new JTextArea();
 		cacheArea.setEditable(false);
 		cacheArea.setBounds(0, 0, 350, 220);
 		arpCacheEditorPanel.add(cacheArea);// chatting edit
-		
+
 		itemDeleteButton = new JButton("Item Delete");
 		itemDeleteButton.setBounds(70, 250, 100, 30);
-		
+
 		allDeleteButton = new JButton("All Delete");
 		allDeleteButton.setBounds(200, 250, 100, 30);
-		/*add Action Listener for delete button*/
+		/* add Action Listener for delete button */
 		arpCachePanel.add(itemDeleteButton);
 		arpCachePanel.add(allDeleteButton);
-		
+
 		dstIpLabel = new JLabel("IP_Addr");
 		dstIpLabel.setBounds(15, 300, 100, 20);
 		arpCachePanel.add(dstIpLabel);
-		
+
 		dstIpWrite = new JTextField();
 		dstIpWrite.setBounds(70, 300, 200, 20);// 249
 		arpCachePanel.add(dstIpWrite);
 		dstIpWrite.setColumns(10);// target ip address writing area
 		dstIpSendButton = new JButton("Send");
 		dstIpSendButton.addActionListener(new setAddressListener());
-		dstIpSendButton.setBounds(285,300,70,20);
+		dstIpSendButton.setBounds(285, 300, 70, 20);
 		arpCachePanel.add(dstIpSendButton);
-		
-		// chatting panel
-		JPanel chattingPanel = new JPanel();
-		chattingPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "chatting",
-				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		chattingPanel.setBounds(10, 380, 360, 276);
-		contentPane.add(chattingPanel);
-		chattingPanel.setLayout(null);
 
-		JPanel chattingEditorPanel = new JPanel();// chatting write panel
-		chattingEditorPanel.setBounds(10, 15, 340, 180);
-		chattingPanel.add(chattingEditorPanel);
-		chattingEditorPanel.setLayout(null);
-
-		ChattingArea = new JTextArea();
-		ChattingArea.setEditable(false);
-		ChattingArea.setBounds(0, 0, 340, 210);
-		chattingEditorPanel.add(ChattingArea);// chatting edit
-
-		JPanel chattingInputPanel = new JPanel();// chatting write panel
-		chattingInputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		chattingInputPanel.setBounds(10, 230, 250, 20);
-		chattingPanel.add(chattingInputPanel);
-		chattingInputPanel.setLayout(null);
-
-		ChattingWrite = new JTextField();
-		ChattingWrite.setBounds(2, 2, 250, 20);// 249
-		chattingInputPanel.add(ChattingWrite);
-		ChattingWrite.setColumns(10);// writing area
-
-		// file panel
-		JPanel fileTransferPanel = new JPanel();// file panel
-		fileTransferPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "file transfer",
-				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		fileTransferPanel.setBounds(380, 380, 350, 90);
-		contentPane.add(fileTransferPanel);
-		fileTransferPanel.setLayout(null);
-
-		JPanel fileEditorPanel = new JPanel();// chatting write panel
-		fileEditorPanel.setBounds(10, 20, 330, 60);
-		fileTransferPanel.add(fileEditorPanel);
-		fileEditorPanel.setLayout(null);
-
-		fileArea = new JTextArea();
-		fileArea.setEditable(false);
-		fileArea.setBounds(0, 5, 250, 20);
-		fileEditorPanel.add(fileArea);// chatting edit
-
-		openFileButton = new JButton("File...");
-		openFileButton.setBounds(260, 5, 70, 20);
-		openFileButton.addActionListener(new setAddressListener());
-		fileEditorPanel.add(openFileButton);
-
-		this.progressBar = new JProgressBar(0, 100);
-		this.progressBar.setBounds(0, 40, 250, 20);
-		this.progressBar.setStringPainted(true);
-		fileEditorPanel.add(this.progressBar);
-
-		File_send_Button = new JButton("전송");
-		File_send_Button.setBounds(260, 40, 70, 20);
-		fileEditorPanel.add(File_send_Button);
-		File_send_Button.addActionListener(new setAddressListener());
-		File_send_Button.setEnabled(false);
-		
-		// proxy arp entry panel 
+		// proxy arp entry panel
 		JPanel proxyArpPanel = new JPanel();
 		proxyArpPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Proxy Arp Entry",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		proxyArpPanel.setBounds(380, 5, 350, 370);
 		contentPane.add(proxyArpPanel);
 		proxyArpPanel.setLayout(null);
-		
+
 		JPanel proxyEditorPanel = new JPanel();// proxy editor panel
 		proxyEditorPanel.setBounds(5, 15, 330, 160);
 		proxyArpPanel.add(proxyEditorPanel);
 		proxyEditorPanel.setLayout(null);
-		
+
 		proxyArpArea = new JTextArea();
 		proxyArpArea.setEditable(false);
 		proxyArpArea.setBounds(5, 5, 420, 150);
 		proxyEditorPanel.add(proxyArpArea);// proxy arp entry
-		
+
 		JPanel proxyInputPanel = new JPanel();
-		proxyInputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED,null,null,null,null));
-		proxyInputPanel.setBounds(10,200,320,150);
+		proxyInputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		proxyInputPanel.setBounds(10, 200, 320, 150);
 		proxyInputPanel.setLayout(null);
 		proxyArpPanel.add(proxyInputPanel);
-		
-		proxyDevice= new JLabel("Device");
+
+		proxyDevice = new JLabel("Device");
 		proxyDevice.setBounds(20, 10, 60, 20);
 		proxyInputPanel.add(proxyDevice);
-		
+
 		proxyIp = new JLabel("IP 주소");
-		proxyIp.setBounds(20,40,60,20);
+		proxyIp.setBounds(20, 40, 60, 20);
 		proxyInputPanel.add(proxyIp);
-		
+
 		proxyMac = new JLabel("Mac 주소");
-		proxyMac.setBounds(20,70,60,20);
+		proxyMac.setBounds(20, 70, 60, 20);
 		proxyInputPanel.add(proxyMac);
-		
+
 		proxyDeviceWrite = new JTextField();
-		proxyDeviceWrite.setBounds(100,10,200,20);
+		proxyDeviceWrite.setBounds(100, 10, 200, 20);
 		proxyInputPanel.add(proxyDeviceWrite);
-		
+
 		proxyIpWrite = new JTextField();
-		proxyIpWrite.setBounds(100,40,200,20);
+		proxyIpWrite.setBounds(100, 40, 200, 20);
 		proxyInputPanel.add(proxyIpWrite);
-		
+
 		proxyMacWrite = new JTextField();
-		proxyMacWrite.setBounds(100,70,200,20);
+		proxyMacWrite.setBounds(100, 70, 200, 20);
 		proxyInputPanel.add(proxyMacWrite);
-		
+
 		proxyAddButton = new JButton("Add");
-		proxyAddButton.setBounds(70,100,80,30);
+		proxyAddButton.setBounds(70, 100, 80, 30);
 		proxyDeleteButton = new JButton("Delete");
-		proxyDeleteButton.setBounds(180,100,80,30);
+		proxyDeleteButton.setBounds(180, 100, 80, 30);
 		proxyInputPanel.add(proxyAddButton);
 		proxyInputPanel.add(proxyDeleteButton);
 		
-		// gratuitous panel 
+		proxyAddButton.addActionListener(new setAddressListener());
+		proxyDeleteButton.addActionListener(new setAddressListener());
+		
+		// gratuitous panel
 		JPanel gratuitousPanel = new JPanel();
 		gratuitousPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Gratuitous ARP",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		gratuitousPanel.setBounds(730, 5, 250, 200);
 		contentPane.add(gratuitousPanel);
 		gratuitousPanel.setLayout(null);
-		
+
 		GArpAddress = new JLabel("H/W address");
-		GArpAddress.setBounds(10,30,100,10);
+		GArpAddress.setBounds(10, 30, 100, 10);
 		GArpAddressWrite = new JTextField();
-		GArpAddressWrite.setBounds(20,60,200,20);
+		GArpAddressWrite.setBounds(20, 60, 200, 20);
 		GArpSendButton = new JButton("전송");
-		GArpSendButton.setBounds(70,100,100,30);
+		GArpSendButton.setBounds(70, 100, 100, 30);
 		gratuitousPanel.add(GArpAddress);
 		gratuitousPanel.add(GArpAddressWrite);
 		gratuitousPanel.add(GArpSendButton);
-		
+
 		// setting panel
 		JPanel settingPanel = new JPanel();
 		settingPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "setting",
@@ -462,6 +415,68 @@ public class ARPDlg extends JFrame implements BaseLayer {
 			e1.printStackTrace();
 		}
 		;
+		// chatting panel
+		JPanel chattingPanel = new JPanel();
+		chattingPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "chatting",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		chattingPanel.setBounds(10, 380, 360, 276);
+		contentPane.add(chattingPanel);
+		chattingPanel.setLayout(null);
+
+		JPanel chattingEditorPanel = new JPanel();// chatting write panel
+		chattingEditorPanel.setBounds(10, 15, 340, 180);
+		chattingPanel.add(chattingEditorPanel);
+		chattingEditorPanel.setLayout(null);
+
+		ChattingArea = new JTextArea();
+		ChattingArea.setEditable(false);
+		ChattingArea.setBounds(0, 0, 340, 210);
+		chattingEditorPanel.add(ChattingArea);// chatting edit
+
+		JPanel chattingInputPanel = new JPanel();// chatting write panel
+		chattingInputPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		chattingInputPanel.setBounds(10, 230, 250, 20);
+		chattingPanel.add(chattingInputPanel);
+		chattingInputPanel.setLayout(null);
+
+		ChattingWrite = new JTextField();
+		ChattingWrite.setBounds(2, 2, 250, 20);// 249
+		chattingInputPanel.add(ChattingWrite);
+		ChattingWrite.setColumns(10);// writing area
+
+		// file panel
+		JPanel fileTransferPanel = new JPanel();// file panel
+		fileTransferPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "file transfer",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		fileTransferPanel.setBounds(380, 380, 350, 90);
+		contentPane.add(fileTransferPanel);
+		fileTransferPanel.setLayout(null);
+
+		JPanel fileEditorPanel = new JPanel();// chatting write panel
+		fileEditorPanel.setBounds(10, 20, 330, 60);
+		fileTransferPanel.add(fileEditorPanel);
+		fileEditorPanel.setLayout(null);
+
+		fileArea = new JTextArea();
+		fileArea.setEditable(false);
+		fileArea.setBounds(0, 5, 250, 20);
+		fileEditorPanel.add(fileArea);// chatting edit
+
+		openFileButton = new JButton("File...");
+		openFileButton.setBounds(260, 5, 70, 20);
+		openFileButton.addActionListener(new setAddressListener());
+		fileEditorPanel.add(openFileButton);
+
+		this.progressBar = new JProgressBar(0, 100);
+		this.progressBar.setBounds(0, 40, 250, 20);
+		this.progressBar.setStringPainted(true);
+		fileEditorPanel.add(this.progressBar);
+
+		File_send_Button = new JButton("전송");
+		File_send_Button.setBounds(260, 40, 70, 20);
+		fileEditorPanel.add(File_send_Button);
+		File_send_Button.addActionListener(new setAddressListener());
+		File_send_Button.setEnabled(false);
 
 		Setting_Button = new JButton("Setting");// setting
 		Setting_Button.setBounds(80, 180, 100, 20);
@@ -476,7 +491,7 @@ public class ARPDlg extends JFrame implements BaseLayer {
 		setVisible(true);
 
 	}
-	
+
 	public File getFile() {
 		return this.file;
 	}
